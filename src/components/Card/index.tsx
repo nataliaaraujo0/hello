61606502ce85bccb4f08ea93e753a22be5fc7d31
Natalia <natalia.araujo@yazo.com.br>
2022-09-15 01:26:59 -0300

    saudacao por horario

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,18 +2,11 @@ export const Card = () => {
   const data = new Date();
   const time = data.getHours();
 
-  function salutation() {
-    if (time >= 0 && time <= 11) {
-      alert("Bom dia");
-    } else if (time >= 12 && time <= 17) {
-      alert("boa tarde");
-    } else if (time >= 18 && time <= 23) {
-      alert("boa noite");
-    } else {
-      alert("nao disse nada");
-    }
-  }
-  window.onload = salutation;
-
-  return <h1> a hora atual é: {time}h</h1>;
+  return (
+    <>
+      {time >= 0 && time <= 11 && <strong>Bom dia</strong>}
+      {time >= 12 && time <= 17 && <strong>Boa tarde</strong>}
+      {time >= 18 && time <= 23 && <strong>Boa noite</strong>}
+    </>
+  );
 };
