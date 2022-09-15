8ea8206614698f4e8f5d7fd4b7387afa28d04858
Natalia <natalia.araujo@yazo.com.br>
2022-09-15 01:55:09 -0300

    estrutura incial componente card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,12 +1,41 @@
+import styles from "./styled.css";
+import Summer from "../../../src/assets/summer.jpg";
+import Night from "../../../src/assets/night.jpg";
+import Morning from "../../../src/assets/morning.jpg";
+
 export const Card = () => {
   const data = new Date();
   const time = data.getHours();
 
+  //let time = 18;
   return (
     <>
-      {time >= 0 && time <= 11 && <strong>Bom dia</strong>}
-      {time >= 12 && time <= 17 && <strong>Boa tarde</strong>}
-      {time >= 18 && time <= 23 && <strong>Boa noite</strong>}
+      {time >= 0 && time <= 11 && (
+        <div style={{ width: "600px", height: "600px" }}>
+          <img
+            style={{ width: "600px", height: "600px" }}
+            src={Morning}
+            alt=""
+          />
+          <strong>Bom dia 🙃</strong>
+        </div>
+      )}
+      {time >= 12 && time <= 17 && (
+        <div style={{ width: "600px", height: "600px", background: "" }}>
+          <img
+            style={{ width: "600px", height: "600px" }}
+            src={Summer}
+            alt=""
+          />
+          <strong>Boa tarde 🙃</strong>
+        </div>
+      )}
+      {time >= 18 && time <= 23 && (
+        <div style={{ width: "600px", height: "600px" }}>
+          <img style={{ width: "600px", height: "600px" }} src={Night} alt="" />
+          <strong>Boa noite 🙃</strong>
+        </div>
+      )}
     </>
   );
 };
