e92f2798fa8a952876c41ff9f9694b0ff7df60d4
Natalia <natalia.araujo@yazo.com.br>
2022-09-15 01:59:57 -0300

    isloando responsabilidade de saudacao em consts

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -6,11 +6,15 @@ import Morning from "../../../src/assets/morning.jpg";
 export const Card = () => {
   const data = new Date();
   const time = data.getHours();
-
   //let time = 18;
+
+  const goodMorning = time >= 0 && time <= 11;
+  const goodAfternoon = time >= 12 && time <= 17;
+  const goodEvening = time >= 18 && time <= 23;
+
   return (
     <>
-      {time >= 0 && time <= 11 && (
+      {goodMorning && (
         <div style={{ width: "600px", height: "600px" }}>
           <img
             style={{ width: "600px", height: "600px" }}
@@ -20,7 +24,7 @@ export const Card = () => {
           <strong>Bom dia 🙃</strong>
         </div>
       )}
-      {time >= 12 && time <= 17 && (
+      {goodAfternoon && (
         <div style={{ width: "600px", height: "600px", background: "" }}>
           <img
             style={{ width: "600px", height: "600px" }}
@@ -30,7 +34,7 @@ export const Card = () => {
           <strong>Boa tarde 🙃</strong>
         </div>
       )}
-      {time >= 18 && time <= 23 && (
+      {goodEvening && (
         <div style={{ width: "600px", height: "600px" }}>
           <img style={{ width: "600px", height: "600px" }} src={Night} alt="" />
           <strong>Boa noite 🙃</strong>
